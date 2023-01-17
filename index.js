88d33af7443febc32deafa72ff2813d719771789
Rio <onwukachibike@gmail.com>
2023-01-17 09:23:51 +0100

    updated

--- index.js
+++ index.js
@@ -1,75 +1,58 @@
-async  function  connect() {
-    /** MetaMask injects a global API into websites visited by its users at `window.ethereum`. This API allows websites to request users' Ethereum accounts, read data from blockchains the user is connected to, and suggest that the user sign messages and transactions. The presence of the provider object indicates an Ethereum user. Read more: https://ethereum.stackexchange.com/a/68294/85979**/
-    
-    // Check if MetaMask is installed, if it is, try connecting to an account
-        if (typeof  window.ethereum !== "undefined") {
-            try {
-                console.log("connecting");
-                // Requests that the user provides an Ethereum address to be identified by. The request causes a MetaMask popup to appear. Read more: https://docs.metamask.io/guide/rpc-api.html#eth-requestaccounts
-                await  ethereum.request({ method:  "eth_requestAccounts" });
-            } catch (error) {
-            console.log(error);
-            }
-            // If connected, change button to "Connected"
-            document.getElementById("login_button").innerHTML = "Connected";
-            // If connected, enable "Swap" button
-            document.getElementById("swap_button").disabled = false;
-            } 
-            // Ask user to install MetaMask if it's not detected 
-            else {
-            document.getElementById("login_button").innerHTML =
-                "Please install MetaMask";
-            }
-        }
-    // Call the connect function when the login_button is clicked
-    document.getElementById("login_button").onclick = connect;
-
-document.getElementById("from_token_select").onclick = openModal;
-function  openModal(){
-    document.getElementById("token_modal").style.display = "block";
-}
-document.getElementById("modal_close").onclick = closeModal;
-function  closeModal(){
-    document.getElementById("token_modal").style.display = "none";
-}
-
-async function init(){
-    console.log("initializing");
-    let response = await fetch('https://tokens.coingecko.com/uniswap/all.json');
-    let tokenListJSON = await response.json();
-    console.log("listing available tokens: ", tokenListJSON);
-}
-
-
-// Add init() call
-init();
-
-document.getElementById("login_button").onclick = connect;
-document.getElementById("from_token_select").onclick = openModal;
-document.getElementById("to_token_select").onclick = openModal;
-document.getElementById("modal_close").onclick = closeModal;
-
-async function listAvailableTokens(){
-    console.log("initializing");
-    let response = await fetch('https://tokens.coingecko.com/uniswap/all.json');
-    let tokenListJSON = await response.json();
-    console.log("listing available tokens: ", tokenListJSON);
-    tokens = tokenListJSON.tokens
-    console.log("tokens:", tokens);
-  
-    // Create a token list for the modal
-    let parent = document.getElementById("token_list");
-    // Loop through all the tokens inside the token list JSON object
-    for (const i in tokens){
-      // Create a row for each token in the list
-      let div = document.createElement("div");
-      div.className = "token_row";
-      // For each row, display the token image and symbol
-      let html = `
-      <img class="token_list_img" src="${tokens[i].logoURI}">
-        <span class="token_list_text">${tokens[i].symbol}</span>
-        `;
-      div.innerHTML = html;
-      parent.appendChild(div);
-    }
+// index.js
+async function init() {
+    await listAvailableTokens();
   }
+  
+  async function listAvailableTokens(){
+    console.log("initializing");
+    let response = await fetch('https://tokens.coingecko.com/uniswap/all.json');
+    let tokenListJSON = await response.json();
+    console.log("listing available tokens: ", tokenListJSON);
+    tokens = tokenListJSON.tokens;
+    console.log("tokens: ", tokens);
+  
+    // Create token list for modal
+    let parent = document.getElementById("token_list");
+    for (const i in tokens){
+        // Token row in the modal token list
+        let div = document.createElement("div");
+        div.className = "token_row";
+        let html = `
+        <img class="token_list_img" src="${tokens[i].logoURI}">
+          <span class="token_list_text">${tokens[i].symbol}</span>
+          `;
+        div.innerHTML = html;
+        parent.appendChild(div);
+    };
+  }
+  
+  async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            console.log("connecting");
+            await ethereum.request({ method: "eth_requestAccounts" });
+        } catch (error) {
+            console.log(error);
+        }
+        document.getElementById("login_button").innerHTML = "Connected";
+        // const accounts = await ethereum.request({ method: "eth_accounts" });
+        document.getElementById("swap_button").disabled = false;
+        } else {
+        document.getElementById("login_button").innerHTML =
+            "Please install MetaMask";
+        }
+    }
+  
+    init();
+  
+    function openModal(){
+        document.getElementById("token_modal").style.display = "block";
+    }
+  
+    function closeModal(){
+    document.getElementById("token_modal").style.display = "none";
+    }
+  
+    document.getElementById("login_button").onclick = connect;
+    document.getElementById("from_token_select").onclick = openModal;
+    document.getElementById("modal_close").onclick = closeModal;
